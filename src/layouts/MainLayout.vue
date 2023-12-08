08c47880c515c380571566ea28491197b9b8b986
ahaocoder <ahao.coder@gmail.com>
2023-12-08 18:39:14 +0800

    test:test

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">首页</RouterLink>
+          <RouterLink to="/about">反诈资讯</RouterLink>
+          <RouterLink to="/about"></RouterLink>
+          <RouterLink to="/about">反诈资讯</RouterLink>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <router-view/>
+    </main>
+    <footer>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {RouterLink} from "vue-router";
+
+</script>
+
+<style scoped>
+
+</style>
